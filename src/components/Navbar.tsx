f6d5600463cd705627e61f45349d41f90efb348d
Joylinton04 <adujoy05@gmail.com>
2025-02-02 06:12:54 +0000

    navbar, home setup

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
+  const navItems = [
+    {nav: 'Home', to: ''},
+    {nav: 'Effectiveness', to: 'effectiveness'},
+    {nav: 'Future Prospect', to: 'future_prospect'},
+    {nav: 'Strengthen', to: 'strength'},
+    {nav: 'About', to: 'about'},
+  ]
   return (
-    <div>
-      
+    <div className="flex justify-between items-center py-8">
+      <h1 className="font-extrabold font-Poppins text-[#f5f5f5]">
+        <span></span>
+        <span className="text-5xl">Xfarm</span>
+      </h1>
+      <ul className="flex gap-8 font-semibold text-[#ccc] font-Poppins">
+        {navItems.map((item, index) => (
+          <li key={index}>
+            <NavLink to={`/${item.to}`}>{item.nav}</NavLink>
+          </li>
+        ))}
+      </ul>
+      <h1 className="px-8 py-3 bg-green-700 rounded-full font-medium cursor-pointer">Learn more</h1>
     </div>
   )
 }
